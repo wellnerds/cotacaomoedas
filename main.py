ff78f4ec90779ffd72600c2a10bdf64b298fde44
wellington ferreira <well_jua@hotmail.com>
2022-11-20 23:28:39 -0300

    Calculo de moeda

---
+++ main.py
@@ -0,0 +1,13 @@
+import httpx
+
+base_currency = input('Digite a moeda de base:').upper()
+currency = input('Digite a moeda desejada: ').upper()
+
+
+response = httpx.get(
+    url=f'https://api.exchangerate.host/latest?base={base_currency}'
+).json()
+currency_data = response['rates']
+
+print(f'1 {base_currency} vale {currency_data.get(currency)} {currency}')
+
